9865b309fe9210b5115b40796d6dd16ace2006b7
chenzw <656469722@qq.com>
2019-06-03 23:51:19 +0800

    initialization

--- spring-mvc-xml-samples/src/main/java/cn/chenzw/springmvc/xml/support/parser/SSOTemplateScanBeanDefinitionParser.java
+++ spring-mvc-xml-samples/src/main/java/cn/chenzw/springmvc/xml/support/parser/SSOTemplateScanBeanDefinitionParser.java
@@ -1,22 +1,34 @@
 package cn.chenzw.springmvc.xml.support.parser;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Element;
 
-public class SSOTemplateScanBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+import java.util.Arrays;
+import java.util.Set;
 
+public class SSOTemplateScanBeanDefinitionParser implements BeanDefinitionParser {
     @Override
-    protected Class<?> getBeanClass(Element element) {
+    public BeanDefinition parse(Element element, ParserContext parserContext) {
 
-        System.out.println("-------------------");
-        return super.getBeanClass(element);
+        String basePackage = element.getAttribute("base-package");
+        basePackage = parserContext.getReaderContext().getEnvironment().resolvePlaceholders(basePackage);
+
+        String[] basePackages = StringUtils.tokenizeToStringArray(basePackage, ",; \t\n");
+
+
+        /*ClassPathBeanDefinitionScanner scanner = this.configureScanner(parserContext, element);
+        Set<BeanDefinitionHolder> beanDefinitions = scanner.doScan(basePackages);
+        this.registerComponents(parserContext.getReaderContext(), beanDefinitions, element);*/
+        return null;
     }
 
-    @Override
-    protected void doParse(Element element, ParserContext parserContext, BeanDefinitionBuilder builder) {
-        super.doParse(element, parserContext, builder);
-        System.out.println("----------11-----");
-    }
+
+
+
 }
